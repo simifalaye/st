1b8bec0daa67c8796bd7a2129b27423593fe150f
Simi Falaye <Simi.Falaye@vecima.com>
2020-03-16 11:09:37 -0600

    iosevka font

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "MesloLGS NF:pixelsize=15:antialias=true:autohint=true";
+static char *font = "Iosevka Term:pixelsize=17:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
